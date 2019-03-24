30e9757c07a726ca126c9e998df54b0fac9c3d4f
unknown <imran.ahmedani@gmail.com>
2019-03-24 05:47:56 -0700

    session view fixed

--- migrations/20190128163134_Screening.js
+++ migrations/20190128163134_Screening.js
@@ -715,6 +715,24 @@ FROM   [main].[tblSessions]
       OR  [main].[v_geo].[uc_id] = [main].[tblSessions].[uc_id]
 WHERE  [tblsessions].[is_deleted] = 0;`
     )
+    .raw(`CREATE VIEW [vSessionsFullForUpdate]
+    AS
+    SELECT 
+           [main].[v_geo].[province], 
+           [main].[v_geo].[province_id], 
+           [main].[v_geo].[district_id], 
+           [main].[v_geo].[district_name], 
+           [main].[v_geo].[tehsil_id], 
+           [main].[v_geo].[tehsil_name], 
+           [main].[v_geo].[uc_id], 
+           [main].[v_geo].[uc_name], 
+           [main].[v_geo].[site_name], 
+           [tblSessions].*
+    FROM   [main].[tblSessions]
+           INNER JOIN [main].[v_geo] ON [main].[v_geo].[site_id] = [main].[tblSessions].[site_id]
+           OR  [main].[v_geo].[uc_id] = [main].[tblSessions].[uc_id]
+    WHERE  [tblsessions].[is_deleted] = 0;
+    `)
     .raw(
       `CREATE VIEW [vStockDistReport]
       AS
